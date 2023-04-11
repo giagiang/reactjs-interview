d44c54089121cde6e5e494fd6341a289f2bb7e66
fastboy <giagiang200@gmail.com>
2023-04-11 21:58:10 +0700

    challangesqueue

--- src/App.js
+++ src/App.js
@@ -307,19 +307,94 @@
 
 
 
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function Counter(){
-  let[number, setNumber]  = useState(0);
-  return( 
-  <>
-    <h1>{number}</h1>
-    <button onClick={() => {
-        setNumber(number + 5);
-        setNumber(n => n + 1 );
-        setNumber(10101010101)
+// export default function Counter(){
+//   let[number, setNumber]  = useState(0);
+//   return( 
+//   <>
+//     <h1>{number}</h1>
+//     <button onClick={() => {
+//         setNumber(number + 5);
+//         setNumber(n => n + 1 );
+//         setNumber(10101010101)
 
-    }}>+ 3</button>
-  </>
-  )
+//     }}>+ 3</button>
+//   </>
+//   )
+// }
+
+import { getFinalState } from './processQueue.js';
+
+function increment(n) {
+  return n + 1;
+}
+increment.toString = () => 'n => n+1';
+
+export default function App() {
+  return (
+    <>
+      <TestCase
+        baseState={0}
+        queue={[1, 1, 1]}
+        expected={1}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          increment,
+          increment,
+          increment
+        ]}
+        expected={3}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          5,
+          increment,
+        ]}
+        expected={6}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          5,
+          increment,
+          42,
+          ]}
+          expected={42}
+      />
+    </>
+  );
+}
+
+function TestCase({
+  baseState,
+    queue, 
+  expected
+}) {
+  const actual = getFinalState(baseState, queue);
+  return (
+    <>
+      <p>Base state: <b>{baseState}</b></p>
+      <p>Queue: <b>[{queue.join(', ')}]</b></p>
+      <p>Expected result: <b>{expected}</b></p>
+      <p style={{
+        color: actual === expected ?
+          'green' :
+          'red'
+      }}>
+        Your result: <b>{actual}</b>
+        {' '}
+        ({actual === expected ?
+          'correct' :
+          'wrong'
+        })
+      </p>
+    </>
+  );
 }
