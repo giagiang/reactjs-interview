61888d5721a2feba8ce11225fc1244e046cea137
fastboy <giagiang200@gmail.com>
2023-05-10 16:45:51 +0700

    promies preview

--- src/App.js
+++ src/App.js
@@ -1,524 +1,173 @@
-// import { useState } from 'react';
 
-// let nextId = 0;
-
-// export default function List() {
-//   const [name, setName] = useState('');
-//   const [artists, setArtists] = useState([]);
-
-//   return (
-//     <>
-//       <h1>Inspiring sculptors:</h1>
-//       <input
-//         value={name}
-//         onChange={e => setName(e.target.value)}
-//       />
-//       <button onClick={() => {
-//         setArtists([
-//           ...artists,
-//           { id: nextId ++, name: name }
-//         ]);
-//         console.log(artists)
-//       }}>Add</button>
-//       <ul>
-//         {artists.map(artist => (
-//           <li key={artist.id}>{artist.name}</li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// }
-
-
-// import {useState} from 'react'; 
-// let initialArtists  = [
-//     {
-//         id: 0,  name: 'Marta  Colvin Anfrade'
-//     },
-//     {
-//         id: 1,  name: 'Lamidi Olonade Fakeye'
-//     },
-//     {
-//         id: 2,  name: 'Louise Nevelson'
-//     }
-
-// ]
-// export default function List(){
-//     const[ artists, setArtists] = useState(initialArtists)
-
-// return ( 
-//     <>
-//         <h1>Inspiring  sculptors:</h1>
-//         <ul>
-//             {artists.map(artist => (
-//             <li key = {artist.id}> {artist.name}{' '}  
-//             <button onClick={()=>{
-//                 setArtists(
-//                     artists.filter(a => a.id !== artist.id));
-//             }}>delete </button></li>
-//             ))}
-//         </ul>
-//     </>
-// )
-// };
-
-
-// import { useState } from 'react';
-
-// let initialShapes = [
-//   { id: 0, type: 'circle', x: 50, y: 100 },
-//   { id: 1, type: 'square', x: 150, y: 100 },
-//   { id: 2, type: 'circle', x: 250, y: 100 },
-// ];
-
-// export default function ShapeEditor() {
-//   const [shapes, setShapes] = useState(
-//     initialShapes
-//   );
-
-//   function handleClick() {
-//     const nextShapes = shapes.map(shape => {
-//       if (shape.type === 'square') {
-//         // No change
-//         return shape;
-//       } else {
-//         // Return a new circle 50px below
-//         return {
-//           ...shape,
-//           y: shape.y + 50,
-//         };
-//       }
-//     });
-//     // Re-render with the new array
-//     setShapes(nextShapes);
-//   }
-
-//   return (
-//     <>
-//       <button onClick={handleClick}>
-//         Move circles down!
-//       </button>
-//       {shapes.map(shape => (
-//         <div
-//           key={shape.id}
-//           style={{
-//           background: 'purple',
-//           position: 'absolute',
-//           left: shape.x,
-//           top: shape.y,
-//           borderRadius:
-//             shape.type === 'circle'
-//               ? '50%' : '',
-//           width: 20,
-//           height: 20,
-//         }} />
-//       ))}
-//     </>
-//   );
-// }
-
-
-// Replacing items in an array 
-// import  {useState } from 'react' ; 
-// let  initialCountter = [0,0,0];
-// export default function CounterList(){
-//     const [counters, setCounters] = useState(initialCountter)
-
-// function handelIncrementClick (index ){
-//     const nextCounters = counters.map((c,i )=> {
-//         if(i === index ){
-//             return c+ 1 }
-//         else{
-//             return c ;
-//         }
-//     });
-//     setCounters(nextCounters)
-// }
-// return(
-//     <ul>
-//         {counters.map((counter,i )=>(
-//             <li key = {i}>
-//                 {counter}
-//                 <button onClick={()=>{
-//                     handelIncrementClick(i)
-//                 }}>+1</button>
-//             </li>
-//         ))}
-//     </ul>
-//  )
-// };
-
-
-// Adding to an array 
-
-// import {useState} from 'react';
-// let nextId = 0 ; 
-
-// export default function List(){
-//     const  [name , setName ] = useState('');
-//     const [artists , setArtists] = useState([]);
-    
-
-//     return(
-//          <>
-//             <h1>Inspiring sculptors:</h1>
-//             <input  value={name}
-//             onChange  = {e => setName(e.target.value )}/>
-        
-         
-//          <button onClick={() => {
-//         setArtists([
-//           ...artists,
-//           { id: nextId++, name: name }
-//         ]);
-//       }}>Add</button>
-    
-//     <ul>
-//         {artists.map(artists => (
-//             <li key={artists.id}>{artists.name }</li>
-//         ))}
-//     </ul>
-//     </>
-//  );
-// }
-
-// Removing from an array 
-
-
-// import {useState} from 'react'; 
-// let initialArtists = [
-//     { id: 0, name: 'Marta Colvin Andrade' },
-//     { id: 1, name: 'Lamidi Olonade Fakeye'},
-//     { id: 2, name: 'Louise Nevelson'},
-//   ];
-
-//   export default function List(){
-//     const[artists,setArtists] = useState(
-//         initialArtists
-//     )
-//     return(
-//         <>
-//             <h1> REMOVE Inspiring sculptors</h1>
-//             <ul>
-//                 {artists.map(artist=>
-//                 (<li key= {artist.id}>
-//                 {artist.name}<br/>
-//                 <button onClick={()=>{
-//                     setArtists(
-//                         artists.filter(a=>a.id !== artist.id)
-//                     )
-//                 }}>Delete</button>
-//                 </li>))}
-//             </ul>
-//         </>
-//     )
-//   }
+setTimeout(function (){
+  console.log(1);
+},1000);
+console.log(2);
 
 
 
-// transfromming an array  
+var promise = new Promise(
+  function(resolve, reject){
+    resolve();
+  }
+);
+promise  
+  .then(function(){
+    return new Promise(function(resolve){
+      setTimeout(function(){
+        resolve('long time no see');
+      },3000);
+
+    });
+  })  
+  .then(function(data){
+    console.log(data);
+    // return 2;
+ 
+  })
+  .then(function(data){
+    console.log(data)
+  })
+
+  .catch(function(fullcourses){
+    console.log(fullcourses);
+  })
+  .finally(function(){
+    console.log('done dell');
+  })
 
 
-// import {useState } from 'react'; 
+function sleep (ms ){
+  return new Promise(function (resolve) {
+      setTimeout(resolve, ms);
+  });
+}
 
-//  let initialShapes = [
-//     { id: 0, type: 'circle', x: 50, y: 100 },
-//     { id: 1, type: 'square', x: 150, y: 100 },
-//     { id: 2, type: 'circle', x: 250, y: 100 },
-//   ];
-// export default function ShapeEditor(){
-//     const [shapes, setShepes] =  useState(
-//         initialShapes
-//     )
-//     function handelClick  (){
-//         const nextShapes = shapes.map (shape => {
-//             if  (shape.type ==='square'){
-//                 return shape;
-//             }else {
-//                 return  {
-//                     ...shape,
-//                     x : shape.x +50 
-//                 }
-//             }
-//         });
-//         setShepes(nextShapes);
-//     }
-//     return (
-//         <>
-//         <button onClick  =  {handelClick}>
-//         move circles down! 
-//         </button>
-//         {shapes.map (shape =>(
-//             <div
-//           key={shape.id}
-//           style={{
-//           background: 'purple',
-//           position: 'absolute',
-//           left: shape.x,
-//           top: shape.y,
-//           borderRadius:
-//             shape.type === 'circle'
-//               ? '50%' : '',
-//           width: 20,
-//           height: 20,
-//         }} />
-//         ))}
-//         </>
-//     )
-// }
+sleep(1000)
+  .then(function(){
+    console.log(1);
+    return sleep (1000);
+  })
 
-
-// Replacing items in an array 
-
-
-// import {useState } from 'react';
-
-// let initialCounters = [
-//     0, 0, 0
-//   ];
-
-//   export default function CounterList(){
-//     const [counters, setCounters ] = useState(initialCounters)
-  
-//   function handleIncrementClick (index){
-//     const nextCouters = counters.map ((c,i)=> {
-//         if(i === index){ 
-//             return c + 1 ; 
-//         }else{
-//             return c ;
-//         }
-//     });
-//     setCounters(nextCouters);
-//   }
-//   return(
-//     <ul>
-//         {counters.map((counter, i)=>(
-//             <li key = {i}>
-//                 {counter}
-//                 <button onClick = {() => {
-//                     handleIncrementClick(i);
-//                 }}>+1</button>
-//             </li>
-//         ))}
-//     </ul>
-//   );
-// }
-
-
-// Inserting into an array 
-// import { useState } from 'react';
-
-// let nextId = 3;
-// const initialList = [
-//   { id: 0, title: 'Big Bellies' },
-//   { id: 1, title: 'Lunar Landscape' },
-//   { id: 2, title: 'Terracotta Army' },
-// ];
-
-// export default function List() {
-//   const [list, setList] = useState(initialList);
-
-//   function handleClick() {
-//     const nextList = [...list];
-//     nextList.reverse();
-//     setList(nextList);
-//   }
-
-//   return (
-//     <>
-//       <button onClick={handleClick}>
-//         Reverse
-//       </button>
-//       <ul>
-//           {list.map(artwork => {
-//             console.log(artwork);
-//             return 
-//             <li key={artwork.id}>{artwork.title}</li>;
-//           })}
-//       </ul>      
-//     </>
-//   );
-// }
+  .then(function(){
+    console.log(2);
+    return  new Promise((resolve,reject)=>{
+      reject('lỗi rồi');
+    });
+  })
+  sleep(1000)
+  .then(function(){
+    console.log(3); 
+    return sleep (1000);
+  })
+  .then(function(){
+    console.log(4); 
+    return sleep (1000);  
+  })
+  .catch(function(err ){
+    console.log(err);
+  })
 
 
 
-// Making other changes to an array 
+var promise =  Promise.reject('errro');
+
+promise 
+  .then (function(result){
+    console.log('result: ', result);
+  })
+  .catch(function (err){
+    console.log('err: ' ,err);
+  })
 
 
-// import {useState} from 'react';
-// let nextId  = 3 ; 
-// const initialList = [
-//     { id: 0, title: 'Big Bellies' },
-//     { id: 1, title: 'Lunar Landscape' },
-//     { id: 2, title: 'Terracotta Army' },
-//   ];
-// export default function List (){
-//     const[list, setList] = useState(initialList);
-//     function handelClick(){
-//         const nextList = [...list];
-//         nextList.reverse();
-//         setList(nextList);
-//     }
-//     return ( 
-//         <>
-//             <button onClick={handelClick}>
-//                 reverse
-//             </button>
-//             <ul>
-//                 {list.map(artwork=> (
-//                     <li key = {artwork.id}> {artwork.title}</li>
-//                 ))}
-//             </ul>
-//         </>
-//     )
-// }
+var promise1 = new Promise(
+  function(resolve){
+    setTimeout (function(){
+      resolve([1]);
+    },1000);
+  }
+) 
+var promise2 = new Promise(
+  function(resolve){
+    setTimeout (function(){
+      resolve([2,3]);
+    },5000);
+  }
+) 
+
+Promise.all([promise1,promise2])
+  .then(function([result1,result2]){
+      console.log(result1.concat(result2));
+  });
 
 
-// Updating objects inside arrays 
 
-// import { useState } from 'react';
-
-// let nextId = 3;
-// const initialList = [
-//   { id: 0, title: 'Big Bellies', seen: false },
-//   { id: 1, title: 'Lunar Landscape', seen: false },
-//   { id: 2, title: 'Terracotta Army', seen: true },
-// ];
-
-//   export default function BucketList() {
-//   const [myList, setMyList] = useState(initialList);
-//   const [yourList, setYourList] = useState(
-//     initialList
-//   );
-
-//   function handleToggleMyList(artworkId, nextSeen) {
-//     setMyList(myList.map(artwork => {
-//         if (artwork.id === artworkId) {
-//           // Create a *new* object with changes
-//           return { ...artwork, seen: nextSeen };
-//         } else {
-//           // No changes
-//           return artwork;
-//         }
-//       }));
-//   }
-
-//   function handleToggleYourList(artworkId, nextSeen) {
-//         setYourList(yourList.map(artwork => {
-//             if (artwork.id === artworkId){
-//                 return{...artwork,seen:nextSeen};
-//             }else{
-//                 return artwork;
-//             }
-//         } ))
-//   }
-
-//   return (
-//     <>
-//       <h1>Art Bucket List</h1>
-//       <h2>My list of art to see:</h2>
-//       <ItemList
-//         artworks={myList}
-//         onToggle={handleToggleMyList} />
-//       <h2>Your list of art to see:</h2>
-//       <ItemList
-//         artworks={yourList}
-//         onToggle={handleToggleYourList} />
-//     </>
-//   );
-// }
-
-// function ItemList({ artworks, onToggle }) {
-//   return (
-//     <ul>
-//       {artworks.map(artwork => (
-//         <li key={artwork.id}>
-//           <label>
-//             <input
-//               type="checkbox"
-//               checked={artwork.seen}
-//               onChange={e => {
-//                 onToggle(
-//                   artwork.id,
-//                   e.target.checked
-//                 );
-//               }}
-//             />
-//             {artwork.title}
-//           </label>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// write concise update  logic with immer 
+  thực hành promise trong thực tế 
 
 
-import {useState} from 'react'; 
-import {useImmer} from 'use-immer' ;
 
-let nextId = 3 ;
-const mang = [{ id: 0, title: 'Big Bellies', seen: true },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: false }, 
+
+var User = [
+  { 
+    id: 0,
+    name : 'giang',
+  },
+  {
+    id: 1, 
+    name : 'tùng',
+  },
+  {
+    id: 2, 
+    name : 'tiến'
+  },
 ];
 
-export default function Listmang(){
-    const [myList, updateMyList] = useImmer(
-        mang
-    );
-    const [yourList , updateYourList ] = useImmer (
-        mang
-    );
-    function handelClickMylist(id,nextSeen){
-        updateMyList (draft => { 
-            const astWork =  draft.find(a=> a.id === id);
-            astWork.seen = nextSeen;
-        });
-    }
-    function handelClickYourList(astWorkId, nextSeen){
-        updateYourList(draft => {
-            const astWork = draft.find(a => a.id === astWorkId);
-            astWork.seen = nextSeen;
-        })
-    }
-    return (
-        <>
-            <h1>đây là đầu trang</h1>
-            <h2> đây là tiêu đề thứ hai </h2>
-            <ItemList 
-            astWork  = {myList}
-            onTagger= {handelClickMylist}
-            />
-            <h2>yourlos</h2>
-            <ItemList 
-                astWork = {yourList}
-                onTagger = {handelClickYourList}
-            />
-        </>
-    )
-};
-function ItemList ({astWork, onTagger}){
-    return (
-        <ul>
-            {astWork.map(astWork => (
-                <li key  = {astWork.id}>
-                    <label>
-                    <input
-                      type="checkbox"
-                         checked={astWork.seen}
-                          onChange={e => {
-                            onTagger(
-                              astWork.id,
-                         e.target.checked
-                );
-              }}
-            />
-                        {astWork.title}
-                    </label>
-                </li>
-            ))}
-        </ul>
-    )
+
+var Comments = [
+  {
+    id: 1 ,
+    user_id : 1 ,
+    content: "hello xin chaò bạn ",
+  },
+  {
+    id: 2 , 
+    user_id: 2 , 
+    content: "heehehehehehe"
+  }
+];
+
+function getComments (){
+  return  new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(Comments)
+    }, 1000);
+  });
 }
+
+function getUserByIds(userIds){
+  return new Promise(function (resolve){
+        var result  = User.filter(function(user) {
+          return userIds.includes(user.id)
+        }); 
+        resolve(result);
+  },1000)
+} 
+
+getComments()
+      .then(function(comments){
+          var userIds = comments.map(function(comments){
+            return comments.user_id;
+          });
+         return getUserByIds(userIds)
+            .then(function (User){
+               return  {
+                User : User,
+                comments :comments
+               };
+            });
+          })
+         .then(function(User){
+            console.log(User);
+      });
+
