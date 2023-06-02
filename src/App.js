826640267dc653d97cf18330750a59ca72a3c1df
fastboy <giagiang200@gmail.com>
2023-06-02 14:32:32 +0700

    variety in state

--- src/App.js
+++ src/App.js
@@ -1,165 +1,473 @@
-async function handleFormSubmit(e) {
-    e.preventDefault();
-    disable(textarea);
-    disable(button);
-    show(loadingMessage);
-    hide(errorMessage);
-    try {
-      await submitForm(textarea.value);
-      show(successMessage);
-      hide(form);
-    } catch (err) {
-      show(errorMessage);
-      errorMessage.textContent = err.message;
-    } finally {
-      hide(loadingMessage);
-      enable(textarea);
-      enable(button);
-    }
-  }
+
+
+
+// import { useState } from 'react';
+
+// export default function FeedbackForm() {
+//   const [text, setText] = useState('');
+//   const [isSending, setIsSending] = useState(false);
+//   const [isSent, setIsSent] = useState(false);
+
+//   async function handleSubmit(e) {
+//     e.preventDefault();
+//     setIsSending(true);
+//     await sendMessage(text);
+//     setIsSending(false);
+//     setIsSent(true);
+//   }
+
+//   if (isSent) {
+//     return <h1>Thanks for feedback!</h1>
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <p>How was your stay at The Prancing Pony?</p>
+//       <textarea
+//         disabled={isSending}
+//         value={text}
+//         onChange={e => setText(e.target.value)}
+//       />
+//       <br />
+//       <button
+//         disabled={isSending}
+//         type="submit"
+//       >
+//         Send
+//       </button>
+//       {isSending && <p>Sending...</p>}
+//     </form>
+//   );
+// }
+
+// // Pretend to send a message.
+// function sendMessage(text) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve, 2000);
+//   });
+// }
+
+
+
+
+// import { useState } from 'react';
+
+// export default function MovingDot() {
+//   const [position, setPosition] = useState({
+//     x: 0,
+//     y: 0
+//   });
+//   return (
+//     <div
+//       onPointerMove={e => {
+//         setPosition({
+//           x: e.clientX,
+//           y: e.clientY
+//         });
+//       }}
+//       style={{
+//         position: 'relative',
+//         width: '100vw',
+//         height: '100vh',
+//       }}>
+//       <div style={{
+//         position: 'absolute',
+//         backgroundColor: 'red',
+//         borderRadius: '50%',
+//         transform: `translate(${position.x}px, ${position.y}px)`,
+//         left: -10,
+//         top: -10,
+//         width: 20,
+//         height: 20,
+//       }} />
+//     </div>
+//   )
+// }
+
+
+
+
+// import  {useState} from 'react'; 
+// export default function FeedbackForm(){
+//   const [text, setText] = useState('');
+//   const [isSending,setIsSending] = useState(false);
+//   const [isSent, setIsSent] = useState(false);
+
+//   async function handleSubmit(e){
+//     e.preventDefault();
+//     setIsSending(false);
+//     await sendMessage (text);
+//     setIsSending(true);
+//     setIsSent(false);
+//   }
+//   if (isSent) {
+//     return <h1>Thanks for feedback!</h1>
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <p>How was your stay at The Prancing Pony?</p>
+//       <textarea
+//         disabled={isSending}
+//         value={text}
+//         onChange={e => setText(e.target.value)}
+//       />
+//       <br />
+//       <button
+//         disabled={isSending}
+//         type="submit"
+//       >
+//         Send
+//       </button>
+//       {isSending && <p>Sending...</p>}
+//     </form>
+//   );
+// }
+
+// function sendMessage(text) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve, 2000);
+//   });
+// }
+
+
+
+
+// import {useState} from 'react';
+// const initialItems = [
+//   {title: 'pretzels', id:0},
+//   {title: 'crisp seaweed', id: 1 },
+//   {title: 'granola bar ', id : 2}, 
+// ];
   
-  function handleTextareaChange() {
-    if (textarea.value.length === 0) {
-      disable(button);
-    } else {
-      enable(button);
-    }
-  }
+// export default function Menu() {
+//   const [items, setItems] = useState(initialItems);
+//   const [selectedItem, setSelectedItem] = useState(
+//     items[0]
+//   );
+//   function handleItemChange(id, e){
+//     setItems(items.map(item => { 
+//       if(item.id === id){
+//         return {  
+//           ...item,  
+//           title: e.target.value
+//         };
+//       }else{
+//         return item;
+//       }
+//     }));
+//   }
+//   return (
+//      <>
+//       <h2>What's your travel snack?</h2> 
+//       <ul>
+//         {items.map((item, index) => (
+//           <li key={item.id}>
+//             <input
+//               value={item.title}
+//               onChange={e => {
+//                 handleItemChange(item.id, e)
+//               }}
+//             />
+//             {' '}
+//             <button onClick={() => {
+//               setSelectedItem(item);
+//             }}>Choose</button>
+//           </li>
+//         ))}
+//       </ul>
+//       <p>You picked {selectedItem.title}.</p>
+//     </>
+//   )
+// }
+
+// import {useState} from 'react';
+// const initialItems  = [
+//   {title: 'pretzels', id: 0},
+//   {title: 'crispy seaweed', id: 1},
+//   {title: 'granola bar', id: 2},
+// ];
+// export default function Menu(){
+//   const [items, setItems] = useState(initialItems);
+//   const [selectedId, setSelectedId] = useState(0)
+//   const selectedItem = items.find(item => item.id ===selectedId);
+
+//   function handelItemChange(id,e){
+//     setItems(items.map(item=> {
+//       if (item.id === id ){
+//         return {
+//           ...item, 
+//           title: e.target.value,
+
+//         };
+
+//       }else{
+//         return item;
+//       }
+//     }));
+//   }
+//     return (
+//       <>
+//       <h2> what's your travel snack</h2>
+//       <ul>
+//         {items.map((item, index)=> (
+//           <li key={item.id}>
+//             <input
+//               value={item.title}
+//               onChange={e => {
+//                   handelItemChange(item.id,e)
+//               }}
+//             />
+//             {' '}
+//             <button onClick ={()=>{
+//               setSelectedId (item.id);
+//             }}>Choose</button>
+//           </li>
+//         ))}
+//       </ul>
+//       <p>you picked {selectedItem.title}.</p>
+//     </>
+//   )
+// };
   
-  function hide(el) {
-    el.style.display = 'none';
-  }
-  
-  function show(el) {
-    el.style.display = '';
-  }
-  
-  function enable(el) {
-    el.disabled = false;
-  }
-  
-  function disable(el) {
-    el.disabled = true;
-  }
-  
-  function submitForm(answer) {
-    // Pretend it's hitting the network.
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (answer.toLowerCase() == 'istanbul') {
-          resolve();
-        } else {
-          reject(new Error('Good guess but a wrong answer. Try again!'));
-        }
-      }, 1500);
+
+
+// import {useState} from 'react'; 
+// import {initialTravelPlan} from './plances.js';
+
+// function PlaceTree({ place }) {
+//   const childPlaces = place.childPlaces;
+//   console.log("childolace value: ", childPlaces );
+//   // console.log("place value: ", place)
+//   return (
+//     <li>
+//        {place.title}
+//         {childPlaces.length > 0 && (
+//          <ol>
+//            {childPlaces.map(place => (
+//              <PlaceTree key={place.id} place={place} />
+//             ))}
+//          </ol>
+//        )}
+//     </li>
+//   );
+// }
+// export default function TravelPlan() {
+//   const [plan, setPlan] = useState(initialTravelPlan);
+//   const planets = plan.childPlaces;
+//   return (
+//     <>
+//       <h2>Places to visit</h2>
+//       <ol>
+//       {planets.map(place => (
+//           <PlaceTree key={place.id} place={place} />
+//         ))}
+//       </ol>
+//     </>
+//   );
+// }
+
+
+// import {useState} from 'react' ; 
+// import { initialTravelPlan } from './plances.js';
+
+// function PlaceTree({id, placesById}){
+//   const place = placesById[id];
+//   const childIds = place.childIds;
+//   return (
+//     <li>
+//       {place.title}
+//       {childIds.length > 0 && (
+//         <ol>
+//           {childIds.map (childId => (
+//             <PlaceTree 
+//               key = {childId}
+//               id = {childId}
+//               placesById={placesById}    
+//             />
+//           ))}
+//         </ol>
+//       )}
+//     </li>
+//   );
+// }
+// export default function TravelPlan(){
+//   const [plan, setPlan] = useState(initialTravelPlan) ;
+//   const root = plan[0];
+//   const planetIds = root.childIds;
+//   console.log("planets value :", planetIds)
+//   return(
+//     <>
+//       <h2>
+//         place to visit
+//       </h2>
+//       <ol>
+//         {planetIds.map(id => (
+//           <PlaceTree 
+//             key= {id}
+//             id= {id}
+//             placesById={plan}
+//           />
+//         ))}
+//       </ol>
+//     </>
+//   );
+// }
+
+
+// import { useState } from 'react';
+// import { initialTravelPlan } from './places.js';
+
+// export default function TravelPlan() {
+//   const [plan, setPlan] = useState(initialTravelPlan);
+
+//   function handleComplete(parentId, childId) {
+//     console.log("parentid value:", parentId);
+//     const parent = plan[parentId];
+//     // Create a new version of the parent place
+//     // that doesn't include this child ID.
+    
+//     const nextParent = {
+//       ...parent,
+//       childIds: parent.childIds
+//         .filter(id => id !== childId)
+//     };
+//     // Update the root state object...
+//     // console.log("plan value:", plan);
+//     // console.log("next parent value:", nextParent);
+//     // const temoryPlan = {
+//     //   ...plan,
+//     //   [parentId]: nextParent
+//     // };
+//     // console.log("temporaryPlan value:",temoryPlan);
+//     // setPlan(temoryPlan)
+//     setPlan({
+//       ...plan,
+//       // ...so that it has the updated parent.
+//       [parentId]: nextParent
+//     });
+//   }
+
+//   const root = plan[0];
+//   const planetIds = root.childIds;
+//   return (
+//     <>
+//       <h2>Places to visit</h2>
+//       <ol>
+//         {planetIds.map(id => (
+//           <PlaceTree
+//             key={id}
+//             id={id}
+//             parentId={0}
+//             placesById={plan}
+//             onComplete={handleComplete}
+//           />
+//         ))}
+//       </ol>
+//     </>
+//   );
+// }
+
+// function PlaceTree({ id, parentId, placesById, onComplete }) {
+//   const place = placesById[id];
+//   const childIds = place.childIds;
+//   return (
+//     <li>
+//       {place.title}
+//       <button onClick={() => {
+//         onComplete(parentId, id);
+//       }}>
+//         Complete
+//       </button>
+//       {childIds.length > 0 &&
+//         <ol>
+//           {childIds.map(childId => (
+//             <PlaceTree
+//               key={childId}
+//               id={childId}
+//               parentId={id}
+//               placesById={placesById}
+//               onComplete={onComplete}
+//             />
+//           ))}
+//         </ol>
+//       }
+//     </li>
+//   );
+// }
+
+
+import { useImmer } from 'use-immer';
+import { initialTravelPlan } from './places.js';
+
+export default function TravelPlan() {
+  const [plan, updatePlan] = useImmer(initialTravelPlan);
+
+  function handleComplete(parentId, childId) {
+    updatePlan(draft => {
+      // Remove from the parent place's child IDs.  
+      const parent = draft[parentId];
+      parent.childIds = parent.childIds
+        .filter(id => id !== childId);
+        console.log("parent value", parentId);
+        console.log("childids value", childId)
+
+      // Forget this place and all its subtree.
+      deleteAllChildren(childId);
+      function deleteAllChildren(id) {
+        const place = draft[id];
+        place.childIds.forEach(deleteAllChildren);
+        delete draft[id];
+        // console.log("forech value:", place)
+      }
     });
   }
-  
-  let form = document.getElementById('form');
-  let textarea = document.getElementById('textarea');
-  let button = document.getElementById('button');
-  let loadingMessage = document.getElementById('loading');
-  let errorMessage = document.getElementById('error');
-  let successMessage = document.getElementById('success');
-  form.onsubmit = handleFormSubmit;
-  textarea.oninput = handleTextareaChange;
-  
 
-
-
-export default function  Form ({
-  status = 'empty'
-}){
-  if (status === 'success'){
-    return <h1> that's right</h1>
-  }
-  return ( 
+  const root = plan[0];
+  const planetIds = root.childIds;
+  return (
     <>
-      <h2>City quiz </h2>
-      <p>
-        in which city is there  billboard that turns air into drinkable  water ?
-      </p>
-      <form>
-        <textarea  disable  = {
-          status  === 'submitting'
-        }/>
-        <br />
-        <button disable = {
-          status ===' empty' || status === 'submitting'  
-        }/>
-        <button> submit</button>
-        {status === 'Error' && <p className  = "Error">
-          good gess but a wrong  answer.tryagain !
-        </p>}
-      </form>
+      <h2>Places to visit</h2>
+      <ol>
+        {planetIds.map(id => (
+          <PlaceTree
+            key={id}
+            id={id}
+            parentId={0}
+            placesById={plan}
+            onComplete={handleComplete}
+          />
+        ))}
+      </ol>
     </>
-  )
+  );
 }
 
-
-
-import {useState} from  'react'  ; 
-export default function  Form (){
-  const [answer,setAnswer] = useState('');
-  const [error,setError]= useState(null);
-  const [status, setStatus] = useState('typing')
-  if(status === 'success'){
-    return <h1>that's right</h1>
-  }
-  async function handleSubmit (e){
-    e.preventDefault();
-    setStatus('submitting');
-    try{
-      await submitForm(answer);
-      setStatus ('success');
-    }catch(err){
-      setStatus('typing ');
-      setError(err);
-    }
-    function handleTextareaChange(e){
-      setAnswer(e.target.value);
-
-    }
-    return (
-      <>
-         <h2>City quiz</h2>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          disabled={status === 'submitting'}
-        />
-        <br />
-        <button disabled={
-          answer.length === 0 ||
-          status === 'submitting'
-        }>
-          Submit
-        </button>
-        {error !== null &&
-          <p className="Error">
-            {error.message}
-          </p>
-        }
-      </form>
-      </>
-    );
-  }
-  function  submitForm(answer){
-    return new Promise((resolve, reject)=>{
-      setTimeout(()=>{
-        let shouldError  = answer.toLowerCase() !== 'lima'
-        if(shouldError){
-          reject(new Error('good guess but a wrong answer. try again! '));
-        }else{
-          resolve();
-        }
-      },1500);
-    })
-  }
+function PlaceTree({ id, parentId, placesById, onComplete }) {
+  const place = placesById[id];
+  const childIds = place.childIds;
+  return (
+    <li>
+      {place.title}
+      <button onClick={() => {
+        onComplete(parentId, id);
+      }}>
+        Complete
+      </button>
+      {childIds.length > 0 &&
+        <ol>
+          {childIds.map(childId => (
+            <PlaceTree
+              key={childId}
+              id={childId}
+              parentId={id}
+              placesById={placesById}
+              onComplete={onComplete}
+            />
+          ))}
+        </ol>
+      }
+    </li>
+  );
 }
-
 
--- src/index.js
+++ src/index.js
@@ -1,87 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import {Name, AboutPage} from './App';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-async function handleFormSubmit(e) {
-  e.preventDefault();
-  disable(textarea);
-  disable(button);
-  show(loadingMessage);
-  hide(errorMessage);
-  try {
-    await submitForm(textarea.value);
-    show(successMessage);
-    hide(form);
-  } catch (err) {
-    show(errorMessage);
-    errorMessage.textContent = err.message;
-  } finally {
-    hide(loadingMessage);
-    enable(textarea);
-    enable(button);
-  }
-}
-
-function handleTextareaChange() {
-  if (textarea.value.length === 0) {
-    disable(button);
-  } else {
-    enable(button);
-  }
-}
-
-function hide(el) {
-  el.style.display = 'none';
-}
-
-function show(el) {
-  el.style.display = '';
-}
-
-function enable(el) {
-  el.disabled = false;
-}
-
-function disable(el) {
-  el.disabled = true;
-}
-
-function submitForm(answer) {
-  // Pretend it's hitting the network.
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (answer.toLowerCase() == 'istanbul') {
-        resolve();
-      } else {
-        reject(new Error('Good guess but a wrong answer. Try again!'));
-      }
-    }, 1500);
-  });
-}
-
-let form = document.getElementById('form');
-let textarea = document.getElementById('textarea');
-let button = document.getElementById('button');
-let loadingMessage = document.getElementById('loading');
-let errorMessage = document.getElementById('error');
-let successMessage = document.getElementById('success');
-form.onsubmit = handleFormSubmit;
-textarea.oninput = handleTextareaChange;
-
-
