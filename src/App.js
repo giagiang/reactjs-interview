d7f0d307b5adf91e1d020ed0a5cfa3dc176d89d1
fastboy <giagiang200@gmail.com>
2023-07-03 11:07:01 +0700

    Learn/sharing

--- src/App.js
+++ src/App.js
@@ -1,77 +1,77 @@
-// import {useState} from 'react';
+import {useState} from 'react';
   
-// export default function Accordion(){
-// const[activeIndex,setActiveIndex] = useState(0)
-//     return (
-//       <>
-//         <h2>Almaty, Kazakhtan</h2>
-//         <Panel  title = 'About'
-//         isActive = {activeIndex === 0 }
-//         onShow = {() => setActiveIndex(0)}>
-//         With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
-//         </Panel>
-//         <Panel  title = "Etymology"
-//          isActive = {activeIndex === 1}
-//          onShow = {() => setActiveIndex(1)}>
-//         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
-//         </Panel>
-//       </>
-//    );
-// }
-// function Panel({title, children, isActive, onShow}) {
+export default function Accordion(){
+const[activeIndex,setActiveIndex] = useState(0)
+    return (
+      <>
+        <h2>Almaty, Kazakhtan</h2>
+        <Panel  title = 'About'
+        isActive = {activeIndex === 0 }
+        onShow = {() => setActiveIndex(0)}>
+        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+        </Panel>
+        <Panel  title = "Etymology"
+         isActive = {activeIndex === 1}
+         onShow = {() => setActiveIndex(1)}>
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+        </Panel>
+      </>
+   );
+}
+function Panel({title, children, isActive, onShow}) {
 
-//     return(
-//       <section className = "panel">
-//         <h3>{title}</h3>
-//         {isActive ? (
-//           <p>{children}</p>
-//         ) : (
-//           <button onClick = {onShow}> Show </button>
-//         )}
-//       </section>
-//     );
-//   }
+    return(
+      <section className = "panel">
+        <h3>{title}</h3>
+        {isActive ? (
+          <p>{children}</p>
+        ) : (
+          <button onClick = {onShow}> Show </button>
+        )}
+      </section>
+    );
+  }
   
 
 
 
 
-// import {useState} from 'react';
-// export default function  SyncedInputs(){
-//     const [ text, setText] = useState('');
+import {useState} from 'react';
+export default function  SyncedInputs(){
+    const [ text, setText] = useState('');
 
-//     function handelChange (e){
-//       setText(e.target.value );
-//     }
+    function handelChange (e){
+      setText(e.target.value );
+    }
 
-//   return (
-//     <>
-//     <input label  = "First input "
-//       value = {text}
-//       onChange={handelChange}
-//     />
-//     <input label  = "second input "
-//           value = {text}
-//       onChange={handelChange}
-//     />
-//     </>
-//   );
-// }
-//   function  Input ({label, value, onChange}){
+  return (
+    <>
+    <input label  = "First input "
+      value = {text}
+      onChange={handelChange}
+    />
+    <input label  = "second input "
+          value = {text}
+      onChange={handelChange}
+    />
+    </>
+  );
+}
+  function  Input ({label, value, onChange}){
 
 
-//     return (
-//       <label>
-//       {label}
-//       {' '}
-//       <input 
-//         value = {value}
-//         onChange={onChange}
-//       />
+    return (
+      <label>
+      {label}
+      {' '}
+      <input 
+        value = {value}
+        onChange={onChange}
+      />
 
-//       </label>
-//     )
-//   }
+      </label>
+    )
+  }
 
 
 
@@ -125,3 +125,5 @@ function List({ items }) {
     </table>
   );
 }
+
+
