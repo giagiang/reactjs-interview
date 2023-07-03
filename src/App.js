322c28f7beecd6aa5a3e68405202c41edac411ff
fastboy <giagiang200@gmail.com>
2023-07-03 12:29:56 +0700

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,129 +1,26 @@
-import {useState} from 'react';
-  
-export default function Accordion(){
-const[activeIndex,setActiveIndex] = useState(0)
-    return (
-      <>
-        <h2>Almaty, Kazakhtan</h2>
-        <Panel  title = 'About'
-        isActive = {activeIndex === 0 }
-        onShow = {() => setActiveIndex(0)}>
-        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
-        </Panel>
-        <Panel  title = "Etymology"
-         isActive = {activeIndex === 1}
-         onShow = {() => setActiveIndex(1)}>
-        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
-        </Panel>
-      </>
-   );
-}
-function Panel({title, children, isActive, onShow}) {
+import{useRef, useState} from "react";
+function VideoPlayer({src,isPlaying }){
+  const ref = useRef(null);
 
-    return(
-      <section className = "panel">
-        <h3>{title}</h3>
-        {isActive ? (
-          <p>{children}</p>
-        ) : (
-          <button onClick = {onShow}> Show </button>
-        )}
-      </section>
-    );
+  if(isPlaying){
+    ref.current.play();
+  }else{
+    ref.current.pause(); 
   }
-  
+  return<video ref={ref} src={src} loop playsInline/>
+}
 
-
-
-
-import {useState} from 'react';
-export default function  SyncedInputs(){
-    const [ text, setText] = useState('');
-
-    function handelChange (e){
-      setText(e.target.value );
-    }
-
+export default function App(){
+  const [isPlaying,setIsPlaying]=useState(false);
   return (
     <>
-    <input label  = "First input "
-      value = {text}
-      onChange={handelChange}
-    />
-    <input label  = "second input "
-          value = {text}
-      onChange={handelChange}
-    />
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? 'pause' : 'Play'}
+      </button>
+      <VideoPlayer
+        isPlaying={ isPlaying}
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+      />
     </>
   );
 }
-  function  Input ({label, value, onChange}){
-
-
-    return (
-      <label>
-      {label}
-      {' '}
-      <input 
-        value = {value}
-        onChange={onChange}
-      />
-
-      </label>
-    )
-  }
-
-
-
-
-
-import { useState } from 'react';
-import { foods, filterItems } from './data.js';
-
-export default function FilterableList() {
-  const [query, setQuery] = useState('');
-  const result = filterItems(foods, query) ;
-  function handleChange(e) {
-    setQuery(e.target.value);
-  }
-  return (
-    <>
-      <SearchBar 
-        query  = {query}
-        onChange = {handleChange}
-      />
-      <hr />
-      <List items={result} />
-    </>
-  );
-}
-
-function SearchBar({query, onChange}) {
-
-  return (
-    <label>
-      Search:{' '}
-      <input
-        value={query}
-        onChange={onChange}
-      />
-    </label>
-  );
-}
-
-function List({ items }) {
-  return (
-    <table>
-      <tbody>
-        {items.map(food => (
-          <tr key={food.id}>
-            <td>{food.name}</td>
-            <td>{food.description}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
-
-
